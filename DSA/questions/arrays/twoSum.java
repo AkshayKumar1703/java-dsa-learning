6c0cf1ae0aa7b812d1c096ab33a161bceafddf33
Akshaykumar1703 <akshaykumar17030.com>
2026-01-22 22:07:11 +0100

    Two Sum solution for review

--- DSA/questions/arrays/twoSum.java
+++ DSA/questions/arrays/twoSum.java
@@ -1,5 +1,7 @@
-package dsa.questions.arrays;
+package DSA.questions.arrays;
 
+import java.util.*;
+// solved this problem using two pointer
 public class twoSum {
     public int[] TwoSum(int[] numbers, int target) {
         int n = numbers.length;
@@ -16,12 +18,11 @@ public class twoSum {
         }
         return null;
     }
-
     public static void main(String[] args) {
-        twoSum t = new twoSum();
+        DSA.questions.arrays.twoSum t = new DSA.questions.arrays.twoSum();
         int[] numbers = {2,1,7,11};
-     int target = 9;
-     int[] result = t.TwoSum(numbers, target);
-     System.out.println(Arrays.toString(result));
+        int target = 3;
+        int[] result = t.TwoSum(numbers, target);
+        System.out.println(Arrays.toString(result));
     }
 }
