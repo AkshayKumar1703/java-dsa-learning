9f8a4d6993e803eca26c35144ebec23405d6ec6a
Akshaykumar1703 <akshaykumar17030.com>
2026-01-24 07:25:02 +0100

    Code for segregate zero and one

--- DSA/questions/arrays/segregate.java
+++ DSA/questions/arrays/segregate.java
@@ -1,11 +1,13 @@
 package DSA.questions.arrays;
 
 
+import java.util.Arrays;
+
 // Ques 2: Given an array arr consisting of only 0's and 1's in random order.
 // Modify the array in-place to segregate 0s onto the left side
 // and 1s onto the right side of the array.
 public class segregate {
-    void segregate0and1(int[] arr) {
+    int[] segregate0and1(int[] arr) {
         // code here
         int i = 0;
         int j = arr.length-1;
@@ -24,6 +26,14 @@ public class segregate {
                 j--;
             }
         }
+      return arr;
+    }
+    public static void main(String[] args) {
+        segregate s = new segregate();
+        int[] numbers = {0,0,1,1,1,0,1,0,1};
+        int[] result = s.segregate0and1(numbers);
+        System.out.println(Arrays.toString(result));
     }
 }
+
 
