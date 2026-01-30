7cc745a034124d86068abd679c7ccd7810846712
Akshay Kumar <akshaykumar17030@gmail.com>
2026-01-30 16:06:08 +0100

    Rename class from 'segregate' to 'Segregate'

--- DSA/questions/arrays/Segregate.java
+++ DSA/questions/arrays/Segregate.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 // Ques 2: Given an array arr consisting of only 0's and 1's in random order.
 // Modify the array in-place to segregate 0s onto the left side
 // and 1s onto the right side of the array.
-public class segregate {
+public class Segregate {
     int[] segregate0and1(int[] arr) {
         // code here
         int i = 0;
